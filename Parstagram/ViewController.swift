2d153f23bf3388d2a3872585957823a788bae48b
aryasharm01 <sharm388@purdue.edu>
2021-02-24 19:20:25 -0500

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by arya sharma on 2/24/21.
+//  Copyright © 2021 lucky sharm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
